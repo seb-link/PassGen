0894c1b8aed47630855da12098dde4554b5b8984
seb-link <seb974.gamer.pro@gmail.com>
2025-03-14 13:40:33 +0400

    TODO : Add symbols to charset

--- src/password.c
+++ src/password.c
@@ -22,6 +22,7 @@ char* generate_password(charset_t charset, size_t length) {
   char* lower       = NULL; 
   char* upper       = NULL;
   char* numbers     = NULL;
+  char* temp        = NULL;
 
   if (charset & CHAR_LOWER) {
     len += 26;
@@ -38,13 +39,31 @@ char* generate_password(charset_t charset, size_t length) {
   printf("charset len = %ld\n", len);
   charset_str = malloc(len + 1);
   if (lower) {
-    char* temp = malloc(len + 1);
+    temp = malloc(len + 1);
     char* ntemp;
     snprintf(temp, len - 25,"%s", charset_str);
-    snprintf(temp, 27 ,"%s", lower);
+    snprintf(temp, 27 ,"%s%s", temp,lower);
     ntemp = realloc(temp,len);
     snprintf(charset_str, len, "%s", ntemp);
-    free(lower);
+    printf("%s\n",charset_str);
+    free(ntemp);
+  }
+  if (upper) {
+    temp = malloc(len + 1);
+    char* ntemp;
+    snprintf(temp, len - 25,"%s", charset_str);
+    snprintf(temp, len + 1,"%s%s", temp,upper);
+    ntemp = realloc(temp,len);
+    snprintf(charset_str, len + 1, "%s", ntemp);
+    free(ntemp);
+  }
+  if (numbers) {
+    temp = malloc(len + 1);
+    char* ntemp;
+    snprintf(temp, len - 11,"%s", charset_str);
+    snprintf(temp, len + 1,"%s%s", temp,numbers);
+    ntemp = realloc(temp,len);
+    snprintf(charset_str, len + 1, "%s", ntemp);
     free(ntemp);
   }
   puts(charset_str);
